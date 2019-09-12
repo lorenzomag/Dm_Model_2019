9b5469b9765cc9a32adb92832755c9b986543b50
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2019-09-12 18:09:37 +0100

    Update MadAnalysis5 from MadGraph5

--- MG5_aMC_v2_6_6/HEPTools/HEPToolsInstallers/installGINAC_MG5_INTERFACE.sh
+++ MG5_aMC_v2_6_6/HEPTools/HEPToolsInstallers/installGINAC_MG5_INTERFACE.sh
@@ -6,12 +6,12 @@ run () {
 	COMPILER="$3"
 	HEPTOOLSD="$4"
 	cp -R $FOLDERPATH/ $GINAC_MG5_INTERFACE_INSTALLD
-    cd $GINAC_MG5_INTERFACE_INSTALLD
+    cd $GINAC_MG5_INTERFACE_INSTALLD/ginac_mg5_interface
     echo "Setting up ginac_mg5_interface"
     ./ginac_mg5_interface_compile.sh $COMPILER
     echo "Finished installing ginac_mg5_interface."
     echo "Linking the library"
-    ln -s $GINAC_MG5_INTERFACE_INSTALLD/ginac_mg5_interface.so $HEPTOOLSD/lib/ginac_mg5_interface.so
+    ln -s $GINAC_MG5_INTERFACE_INSTALLD/ginac_mg5_interface/ginac_mg5_interface.so $HEPTOOLSD/lib/ginac_mg5_interface.so
     echo "Symbolic link $HEPTOOLSD/lib/ginac_mg5_interface.so created"
    	cd ..
 }
